b03867cdbcae0a05a0a9f714a774dd665d6bb9c8
aoshermes <52600834+aoshermes@users.noreply.github.com>
2019-07-06 21:34:34 +0900

    Create A.cpp

---
+++ 20190427/A.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+  int a, b, t;
+  int sumb;
+
+  cin >> a >> b >> t;
+
+  sumb = (int)( t / a ) * b;
+
+  cout << sumb << endl;
+
+  return 0;
+}
